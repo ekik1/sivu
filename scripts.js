ef7b4d545b14b96bd6102633b83b80240b616f5c
ekik1 <47976129+ekik1@users.noreply.github.com>
2019-02-25 13:41:37 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+/*$.get("http://mtsdo.com/ddjson", function(data, status){
+    $('.loader-5').hide(); 
+    data = JSON.parse(data);
+
+    $( "#leaderboard" ).append("<tr><th>name</th><th>time</th><th>gems</th><th>kills</th></tr>");
+     for(var i = 0; i < data.length; i++){
+         console.log(data[i]);
+         $( "#leaderboard" ).append("<tr><td>" + data[i].name + "</td><td>" + data[i].score + "</td><td>" + data[i].gems + "</td><td>" + data[i].kills +  "</td></tr>");
+     }
+
+});
+*/
+function openPage(pageName, elmnt, color) {
+  
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  
+  elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+
+
+
+
+
